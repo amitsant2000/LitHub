6a27e088315a5513c5ec4951c4855549fd62e942
BrandonHo <brandonho@berkeley.edu>
2021-10-24 00:34:23 -0700

    dfdfdddd

--- eluvioAccess.js
+++ eluvioAccess.js
@@ -1,19 +1,31 @@
 const { ElvClient } = require("@eluvio/elv-client-js");
-
-async function uploadContent(LitType, LitPath, LitSize) {
+const fs = require("fs");
+const myPrivateKey = "0x2d703a925bec7c9847424c2c9b266d0009cb7fb3c00296c9b405241ffeeec858";
+const secondKey = "0xf5ae0c573548597085ee417260510292fcbbcb302c33ee80eb7422902577f2ba";
+const libraryId = "ilib2gZw5kNLYSmmQ76NShfAoYv2Twne";
+async function uploadContent(LitType, LitPath, LitSize, Title, key, Price) {
     const client = await ElvClient.FromConfigurationUrl({
-        configUrl: "https://demov3.net955310.contentfabric.io/config"
+        configUrl: "https://demov3.net955210.contentfabric.io/config"
     });
+    const wallet = client.GenerateWallet();
+  const signer = wallet.AddAccount({
+    privateKey: key
+  });
 
-    const libraryId = "ilib2gZw5kNLYSmmQ76NShfAoYv2Twne";
+client.SetSigner({signer});
     const createResponse = await client.CreateContentObject({libraryId});
     const objectId = createResponse.id;
     const writeToken = createResponse.write_token;
+
     await client.ReplaceMetadata({
       libraryId,
       objectId,
       writeToken,
       metadata: {
+        public: {
+          name: Title,
+          description: Title
+        },
         tags: [
           LitType
         ]
@@ -27,9 +39,10 @@ async function uploadContent(LitType, LitPath, LitSize) {
         fileInfo: [
           {
               path: LitPath,
+              type: "file",
               mime_type: "text/plain",
               size: LitSize,
-              data: ArrayBuffer<String>(LitSize)
+              data: fs.openSync(LitPath,"r")
           }
         ]
     });
@@ -40,6 +53,41 @@ async function uploadContent(LitType, LitPath, LitSize) {
       writeToken
     });
 
+    await client.SetAccessCharge({
+      objectId: objectId, 
+      accessCharge: Price});
+
     const versionHash = finalizeResponse.hash;
     return versionHash
 }
+async function setVisible(objectId, key) {
+  const client = await ElvClient.FromConfigurationUrl({
+    configUrl: "https://demov3.net955210.contentfabric.io/config"
+  });
+  const wallet = client.GenerateWallet();
+  const signer = wallet.AddAccount({
+    privateKey: key
+  });
+  client.SetSigner({signer});
+  await client.SetPermission({objectId: objectId, permission: "viewable"});
+
+  
+}
+async function accessBook(objectId, key) {
+  const client = await ElvClient.FromConfigurationUrl({
+    configUrl: "https://demov3.net955210.contentfabric.io/config"
+  });
+  const wallet = client.GenerateWallet();
+  const signer = wallet.AddAccount({
+    privateKey: key
+  });
+  client.SetSigner({signer});
+  const accessReq =await client.AccessRequest({
+    libraryId: "ilib2gZw5kNLYSmmQ76NShfAoYv2Twne",
+    objectId: objectId,
+  })
+}
+//async function downloadBook()
+//uploadContent("Book", "test.txt", 10000, "Book2", myPrivateKey, 0.05)
+//accessBook("iq__c6TP7cVAqRKSKu4S6PGUqYN36JE", secondKey); 
+setVisible("iq__c6TP7cVAqRKSKu4S6PGUqYN36JE", myPrivateKey);
