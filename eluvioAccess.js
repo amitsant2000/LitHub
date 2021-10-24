19c47891f5bf28b042636914189376dbb1c2a7ca
Brandon Ho <brandonho@berkeley.edu>
2021-10-23 19:09:30 -0700

    wrapped shit in function

--- eluvioAccess.js
+++ eluvioAccess.js
@@ -1,42 +1,48 @@
+
 import { ElvClient } from "elv-client-js/dist/ElvClient-node-min.js";
 const { ElvClient } = require("elv-client-js/dist/ElvClient-node-min.js");
 const client = await ElvClient.FromConfigurationUrl({
     configUrl: "https://demov3.net955310.contentfabric.io/config"
 });
+
+
 const libraryId = "ilib2gZw5kNLYSmmQ76NShfAoYv2Twne";
 const createResponse = await client.CreateContentObject({libraryId});
 const objectId = createResponse.id;
 const writeToken = createResponse.write_token;
 
-await client.ReplaceMetadata({
-  libraryId,
-  objectId,
-  writeToken,
-  metadata: {
-    tags: [
-      "book",
-    ]
-  }
-});
+function uploadContent(LitType, LitPath, LitSize) {
+    await client.ReplaceMetadata({
+      libraryId,
+      objectId,
+      writeToken,
+      metadata: {
+        tags: [
+          LitType
+        ]
+      }
+    });
 
-await client.UploadFiles({
-  libraryId,
-  objectId,
-  writeToken,
-  fileInfo: [
-    {
-      path: "book.pdf",
-      mime_type: "book",
-      size: 10000,
-      data: (<ArrayBuffer 10000>)
-    }
-  ]
-});
+    await client.UploadFiles({
+        libraryId,
+        objectId,
+        writeToken,
+        fileInfo: [
+          {
+              path: LitPath,
+              mime_type: "text/plain",
+              size: LitSize,
+              data: ArrayBuffer<String>(LitSize)
+          }
+        ]
+    });
 
-const finalizeResponse = await client.FinalizeContentObject({
-  libraryId,
-  objectId,
-  writeToken
-});
+    const finalizeResponse = await client.FinalizeContentObject({
+      libraryId,
+      objectId,
+      writeToken
+    });
 
-const versionHash = finalizeResponse.hash;
+    const versionHash = finalizeResponse.hash;
+    return versionHash
+}
